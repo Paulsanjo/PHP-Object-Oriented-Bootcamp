889a61cdb630accc39df1b07e5f564dd622a32af
tisuchi <tisuchi@gmail.com>
2019-04-24 23:28:16 +0600

    Complete Statics and Constants lession.

--- episode7/Math.php
+++ episode7/Math.php
@@ -7,11 +7,17 @@ class Math{
 
         // return $nums;
     }
+
+    public static function addNumbers(...$all){
+        return array_sum($all);
+    }
 }
 
 $obj = new Math();
 
 var_dump($obj->add(1, 2, 3, 4, 5, 6));
+
+echo Math::addNumbers(1,2,3);
 
 /*var_dump(
     $obj->add("My Name", "Hello", "Suchi", "Kochi", "Nazbin", "Nargis", "Sharmin")
